766356d6e97b74fa1afec3c1591e25103be1613c
Thibault Falise <thibault.falise@gmail.com>
2018-02-28 17:36:31 +0100

    Add exercice test

---
+++ projects/linq/linq/Exercises/TotalOrderSum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Linq.Domain;
+
+namespace Linq.Exercises
+{
+    internal static class TotalOrderSum
+    {
+        public static int CalculateTotalOrderSum(Data data)
+        {
+            return -1;
+        }
+    }
+}
